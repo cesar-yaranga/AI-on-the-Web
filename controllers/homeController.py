c0a51b32aafdc158a7d31e90e3e2f7cb1c9ab56f
juliocesar3653 <juliocesar3653@gmail.com>
2022-10-03 15:37:04 -0500

    Some test

--- controllers/homeController.py
+++ controllers/homeController.py
@@ -1,7 +1,7 @@
 import json
 # from models.machine import Inserttable, db
 from services.user_service import insert_logic, create_logic
-from services.computer_vision_service import malla_facial
+from services.computer_vision_service import malla_facial, deteccion_manos
 from flask import render_template, Response
 
 
@@ -10,15 +10,14 @@ def index():
 
 
 def video():
-    return Response(malla_facial(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # return Response(malla_facial(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(deteccion_manos(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # return Response(deteccion_manos(), mimetype='application/json')
 
 
 def create():
-
     create_logic()
 
 
-# insert data into table.
 def insert():
-
     insert_logic()
--- services/computer_vision_service.py
+++ services/computer_vision_service.py
@@ -1,9 +1,10 @@
 import cv2
 import mediapipe as mp
+from flask import redirect
 
 # Realizamos la Videocaptura
-cap = cv2.VideoCapture(0)
-
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 # Mostramos el video en RT
 
 
@@ -48,7 +49,51 @@ def malla_facial():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-# # Ruta del video
-# @app.route('/video')
-# def video():
-#     return Response(gen_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Detector de manos
+def deteccion_manos():
+    mp_drawing = mp.solutions.drawing_utils
+    mp_hands = mp.solutions.hands
+
+    with mp_hands.Hands(
+            static_image_mode=False,
+            max_num_hands=2,
+            min_detection_confidence=0.5) as hands:
+        while True:
+            ret, frame = cap.read()
+
+            if ret == False:
+                break
+
+            height, width, _ = frame.shape
+            frame = cv2.flip(frame, 1)
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            results = hands.process(frame_rgb)
+
+            if results.multi_hand_landmarks is not None:
+                for hand_landmarks in results.multi_hand_landmarks:
+
+                    print(hand_landmarks)
+
+                    mp_drawing.draw_landmarks(
+                        frame, hand_landmarks, mp_hands.HAND_CONNECTIONS,
+                        mp_drawing.DrawingSpec(
+                            color=(0, 255, 255), thickness=3, circle_radius=5),
+                        mp_drawing.DrawingSpec(color=(255, 0, 255), thickness=4, circle_radius=5))
+
+            # Codificamos nuestro video en Bytes
+            suc, encode = cv2.imencode('.jpg', frame)
+            frame = encode.tobytes()
+
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            # yield ('console.log(1)')
+            # yield '<script>document.location.href="http://www.google.com"</script>'
+            # break
+
+    #         cv2.imshow('Frame', frame)
+    #         if cv2.waitKey(1) & 0xFF == 27:
+    #             break
+    # cap.release()
+    # cv2.destroyAllWindows()
